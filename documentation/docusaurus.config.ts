c1ce81136838ce46579c69b37f575aa28cf8f9de
Ravindu <ravinduhiran26@gmail.com>
2026-08-13 11:13:49 +0530

    update the baseURL

--- documentation/docusaurus.config.ts
+++ documentation/docusaurus.config.ts
@@ -53,7 +53,7 @@ const config: Config = {
   url: 'https://wso2.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/agent-manager/',
+  baseUrl: '/',
 
   // Set true for GitHub pages deployment.
   trailingSlash: true,
